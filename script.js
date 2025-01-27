ad7ece7d6aae9afdeb410164d93909bcad3addfb
ritaccfaria <92036100+ritaccfaria@users.noreply.github.com>
2025-01-27 22:30:34 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
 // JavaScript Document
 
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 10;
+    mouseY = e.pageY - 10; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/1);
+    yp += ((mouseY - yp)/1);
+    $(".circle").css({left: xp +'px', top: yp +'px'});
+  }, 0);
+
+});
+
 
 // MUDAR CURSOR ON HOVER
-$('.column').hover(function() {
+$('.column, .column-overlay, nav, #close, #close2').hover(function() {
     $(this).css('cursor','pointer');
+	$(".circle").toggleClass("circle-hover");
+});
+
+	
+
+
+// FECHAR OVERLAYS
+$("#close").on("click", function() {
+ 
+	$(".overlay").toggleClass("overlay-clicked");
+	$("body").css("overflow", "scroll");
+	
+	$('#mob1').get(0).pause();
+    $('#mob1').get(0).currentTime = 0;
+	$('#mob2').get(0).pause();
+    $('#mob2').get(0).currentTime = 0;
+	$('#mob3').get(0).pause();
+    $('#mob3').get(0).currentTime = 0;
+	
+	bindButton6();
+	
+});
+
+$("#close2").on("click", function() {
+ 
+	$(".overlay2").toggleClass("overlay-clicked");
+	$("body").css("overflow", "scroll");
+	
+	$('#vontade1').get(0).pause();
+    $('#vontade1').get(0).currentTime = 0;
+	$('#vontade2').get(0).pause();
+    $('#vontade2').get(0).currentTime = 0;
+	$('#vontade3').get(0).pause();
+    $('#vontade3').get(0).currentTime = 0;
+	
+	bindButton4();
+	
 });
 
 
 
 
 
-
-
-// ABRIR VIDEO FULLSCREEN
-function bindButton() {
-$('.picture').one('click', function() {
-	var video = $("#video1");
-    var newvideo = $("#video1-real");
-
-	console.log("control yourself");
-	video.hide();
-	$(".caption").hide();
-	$(".tag-container").hide();
-	
-	newvideo.show();
-	newvideo.get(0).currentTime = 0;
-	newvideo.get(0).play();
-
-    toggleFullscreen(this); 
-	
-});
-}
-
-bindButton();
-
-// SAIR DO VIDEO FULLSCREEN
 function toggleFullscreen(elem) {
   elem = elem || document.documentElement;
   if (!document.fullscreenElement && !document.mozFullScreenElement &&
@@ -59,7 +89,31 @@ function toggleFullscreen(elem) {
       document.webkitExitFullscreen();
     }
   }
+} 
+
+
+// 	PICTURE BREAK
+function bindButton() {
+$('.picture').one('click', function() {
+	var video = $("#video1");
+    var newvideo = $("#video1-real");
+
+	video.hide();
+	$(".caption").hide();
+	$(".tag-container").hide();
+	$(".laurels").hide();
+	$(".circle").toggleClass("circle-hover");
+	
+	newvideo.show();
+	newvideo.get(0).currentTime = 0;
+	
+	toggleFullscreen(this); 
+	
+});
 }
+
+bindButton();
+
 document.addEventListener('fullscreenchange', exitHandler);
 document.addEventListener('webkitfullscreenchange', exitHandler);
 document.addEventListener('mozfullscreenchange', exitHandler);
@@ -72,11 +126,16 @@ function exitHandler() {
 		video.show();
 		$(".caption").show();
 		$(".tag-container").show();
+		$(".laurels").show();
 		newvideo.hide();	
 		newvideo.get(0).currentTime = 0;
-		newvideo.get(0).pause();
+		newvideo.attr("src","about:blank");
 		bindButton();
 		}
+	
+	newvideo.remove();
+	
+	video.after( "<div class='plyr__video-embed' id='video1-real' style='display: none; object-fit: contain'> <iframe src='https://player.vimeo.com/video/1050185399?h=eeaeff5d79&amp;loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;transparent=0&amp;gesture=media&amp;pip=false&amp;fullscreen=false&amp;vimeo_logo=false&amp;transcript=false&amp;cc=false&amp;speed=false'></iframe></div>" );
 	
 }  
 
@@ -98,9 +157,10 @@ $('.2024').one('click', function() {
 	$(".caption").hide();
 	$(".tag-container").hide();
 	
+	
 	newvideo.show();
 	newvideo.get(0).currentTime = 0;
-	newvideo.attr("src","https://www.youtube.com/embed/8jIdSEORUDs?si=Yit3JEJXBDn7N6Z5&autoplay=1");
+	newvideo.attr("src","https://www.youtube.com/embed/8jIdSEORUDs?si=Yit3JEJXBDn7N6Z5&rel=0");
 
     toggleFullscreen(this); 
 	
@@ -109,7 +169,6 @@ $('.2024').one('click', function() {
 
 bindButton2();
 
-// SAIR DO VIDEO FULLSCREEN
 document.addEventListener('fullscreenchange', exitHandler2);
 document.addEventListener('webkitfullscreenchange', exitHandler2);
 document.addEventListener('mozfullscreenchange', exitHandler2);
@@ -129,6 +188,103 @@ function exitHandler2() {
 		}
 	
 }  
+
+
+
+
+
+
+
+
+
+// ARC
+function bindButton3() {
+$('.arc').one('click', function() {
+	var video = $("#video3");
+    var newvideo = $("#video3-real");
+
+	video.hide();
+	$(".caption").hide();
+	$(".tag-container").hide();
+	$(".circle").toggleClass("circle-hover");
+	
+	newvideo.show();
+	newvideo.get(0).currentTime = 0;
+	
+	toggleFullscreen(this); 
+	
+	
+	
+});
+}
+
+bindButton3();
+
+document.addEventListener('fullscreenchange', exitHandler3);
+document.addEventListener('webkitfullscreenchange', exitHandler3);
+document.addEventListener('mozfullscreenchange', exitHandler3);
+document.addEventListener('MSFullscreenChange', exitHandler3);
+function exitHandler3() {
+    if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+        var video = $("#video3");
+		var newvideo = $("#video3-real");
+	
+		video.show();
+		$(".caption").show();
+		$(".tag-container").show();
+		newvideo.hide();	
+		newvideo.get(0).currentTime = 0;
+		newvideo.attr("src","about:blank");
+		bindButton3();
+		}
+	
+	newvideo.remove();
+	
+	video.after( "<div class='plyr__video-embed' id='video3-real' style='display: none; object-fit: contain'> <iframe src='https://player.vimeo.com/video/76979871?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;transparent=0&amp;gesture=media&amp;pip=false&amp;fullscreen=false&amp;vimeo_logo=false&amp;transcript=false&amp;cc=false&amp;speed=false'></iframe></div>" );
+	
+}  
+
+
+
+
+
+
+
+
+
+
+// MOB
+function bindButton6() {
+$('.mob').one('click', function() {
+	var overlay = $(".overlay");
+
+	overlay.toggleClass("overlay-clicked");
+	$("body").css("overflow", "hidden");
+		
+});
+}
+
+bindButton6();
+
+
+
+
+
+
+
+
+// MA VONTADE
+function bindButton4() {
+$('.vontade').one('click', function() {
+	var overlay = $(".overlay2");
+
+	overlay.toggleClass("overlay-clicked");
+	$("body").css("overflow", "hidden");
+		
+});
+}
+
+bindButton4();
 
 
 
